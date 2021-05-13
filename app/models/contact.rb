4c1009388d76ea28fde601139cbcb52fe9905df1
Brayan Lopez <brayan.lopez@koombea.com>
2021-05-13 15:06:27 -0500

    add a name validation for contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,12 @@ class Contact < ApplicationRecord
 
   validates :telephone, :address, :credit_card,
             :email, presence: true
+
+  def valid_name
+    if self.name.present?
+      self.errors.add(:name, 'bad format field') if self.name.match(/[!$%^&*()_+|~=`{}\[\]:";'<>?,.Ç¨\|@#¢∞¬÷“”≠´\\]/)
+    else
+      self.errors.add(:name, 'Cant be blank')
+    end
+  end
 end
